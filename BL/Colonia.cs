562d1d426ceb73cf5e9f4f0b7d1bc62aebb2f494
LeonardoBlancasU <leonardobu28@outlook.com>
2025-04-16 11:39:26 -0600

    Corección MVC

---
+++ BL/Colonia.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Colonia
+    {
+        public static ML.Result GetByIdMunicipio(int IdMunicipio)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(DL.Conexion.Get()))
+                {
+                    SqlCommand cmd = new SqlCommand("ColoniaGetByIdMunicipio", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@IdMunicipio", IdMunicipio);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+
+                    da.Fill(dataTable);
+                    if (dataTable.Rows.Count > 0)
+                    {
+
+                        result.Objects = new List<object>();
+                        foreach (DataRow row in dataTable.Rows)
+                        {
+                            ML.Colonia colonia = new ML.Colonia();
+
+                            colonia.IdColonia = Convert.ToInt32(row[0].ToString());
+                            colonia.Nombre = (row[1].ToString());
+                            colonia.CodigoPostal = (row[2].ToString());
+
+
+                            result.Objects.Add(colonia);
+                        }
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "No se encontraron Colonias.";
+                    }
+
+                }
+
+
+            }
+
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ BL/Direccion.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Direccion
+    {
+        public static ML.Result AddSP(ML.Usuario usuario)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(DL.Conexion.Get()))
+                {
+                    SqlCommand cmd = new SqlCommand("DireccionAdd", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Calle", usuario.Direccion.Calle);
+                    cmd.Parameters.AddWithValue("@NumeroExterior", usuario.Direccion.NumeroExterior);
+                    cmd.Parameters.AddWithValue("@NumeroInterior", usuario.Direccion.NumeroInterior);
+                    cmd.Parameters.AddWithValue("@IdColonia", usuario.Direccion.Colonia.IdColonia);
+
+                    SqlParameter outIdDireccion = new SqlParameter("IdDireccion", SqlDbType.Int)
+                    {
+                        Direction = ParameterDirection.Output
+                    };
+                    cmd.Parameters.Add(outIdDireccion);
+                    conn.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        result.Object = (int)outIdDireccion.Value;
+                        result.Correct = true;
+
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+
+            return result;
+
+
+        }
+
+        public static ML.Result UpdateSP(ML.Usuario usuario)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(DL.Conexion.Get()))
+                {
+                    SqlCommand cmd = new SqlCommand("DireccionUpdate", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@IdDireccion", usuario.Direccion.IdDireccion);
+                    cmd.Parameters.AddWithValue("@Calle", usuario.Direccion.Calle);
+                    cmd.Parameters.AddWithValue("@NumeroExterior", usuario.Direccion.NumeroExterior);
+                    cmd.Parameters.AddWithValue("@NumeroInterior", usuario.Direccion.NumeroInterior);
+                    cmd.Parameters.AddWithValue("@IdColonia", usuario.Direccion.Colonia.IdColonia);
+                    conn.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+
+            }
+
+            return result;
+
+
+        }
+
+        public static ML.Result GetById(ML.Usuario usuario)
+        {
+            ML.Result result = new ML.Result();
+            int IdDireccion = usuario.Direccion.IdDireccion;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(DL.Conexion.Get()))
+                {
+                    SqlCommand cmd = new SqlCommand("DireccionGetById", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@IdDireccion", IdDireccion);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    da.Fill(dataTable);
+
+                    if (dataTable.Rows.Count > 0)
+                    {
+                        DataRow row = dataTable.Rows[0];
+                        usuario.Direccion.Calle = (row["Calle"].ToString());
+                        usuario.Direccion.NumeroExterior = (row["NumeroExterior"].ToString());
+                        usuario.Direccion.NumeroInterior = (row["NumeroInterior"].ToString());
+                        usuario.Direccion.Colonia = new ML.Colonia();
+                        usuario.Direccion.Colonia.IdColonia = Convert.ToInt32((row["IdColonia"].ToString()));
+
+                        result.Correct = true;
+
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "No se encontro la Direccion.";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+
+            return result;
+        }
+
+        public static ML.Result DeleteSP(int IdDireccion)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(DL.Conexion.Get()))
+                {
+                    SqlCommand cmd = new SqlCommand("DireccionDelete", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@IdDireccion", IdDireccion);
+                    conn.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+
+            return result;
+
+
+        }
+    }
+}
